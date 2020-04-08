adaa3b34cb542e36f2e6049ef66e13a8c42c3adf
krishnachourasia <kchourasia66@gmail.com>
2020-04-08 17:09:12 +0530

    Course App Created with CRUD in all Apps

---
+++ courses/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Course(models.Model):
+
+    class Meta:
+        title = models.CharField(max_length=120)
+
+
+    def get_absolute_url(self):
+        return reverse("courses:course-detail", kwargs={'id':self.id})
